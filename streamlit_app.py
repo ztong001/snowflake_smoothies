ef5145c9648efb2fb125f56c8885d585bd48cf6b
Zack Tong Zi Hang <ztong001@e.ntu.edu.sg>
2024-06-24 21:23:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ ingredients_list= st.multiselect(
 )
 
 if ingredients_list:
-    ingredient_string = ''
+    ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(f'{fruit_chosen} Nutritional Information')
