fee7546b98faf773e32e56ffe8ab10e3b73ea8e5
Zack Tong Zi Hang <ztong001@e.ntu.edu.sg>
2024-06-24 17:07:51 +0800

    Update streamlit_app.py
    Connected changes needed for move from SiS to Streamlit not in Snowflake.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,7 +11,8 @@ st.write(
 )
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothies will be:',name_on_order)
-session = get_active_session()
+session =st.connection("snowflake").session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
