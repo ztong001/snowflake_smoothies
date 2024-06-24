fe35f36ede2553aaba68ab0b76506444b3c86dd5
Zack Tong Zi Hang <ztong001@e.ntu.edu.sg>
2024-06-24 21:26:29 +0800

    Update streamlit_app.py
    add search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
         st.subheader(f'{fruit_chosen} Nutritional Information')
         # New section to display fruityvice nutrition information
         search_on =pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen, 'SEARCH_ON'].iloc[0]
-        fv_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
         fv_df = st.dataframe(data=fv_response.json(),use_container_width=True)
     insert_stmt="""insert into smoothies.public.orders(ingredients,name_on_order) values
     ('"""+ingredients_string+"""','"""+name_on_order+"""')"""
