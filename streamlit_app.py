f1912bf5f4f282e8241de334aa2361bfd022c8aa
Zack Tong Zi Hang <ztong001@e.ntu.edu.sg>
2024-06-24 18:22:21 +0800

    Update streamlit_app.py
    add fruityvice for nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -13,8 +15,10 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothies will be:',name_on_order)
 session =st.connection("snowflake").session()
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+# Convert snowpark dataframe to pandas so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
 
 ingredients_list= st.multiselect(
     'Choose up to 5 ingredients:',
@@ -23,7 +27,13 @@ ingredients_list= st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string= ' '.join(fruit for fruit in ingredients_list)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        st.subheader(f'{fruit_chosen} Nutritional Information')
+        # New section to display fruityvice nutritio n information
+        search_on =pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen, 'SEARCH_ON'].iloc[0]
+        fv_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen+"/nutritions")
+        fv_df = st.dataframe(data=fv_response.json(),use_container_width=True)
     insert_stmt="""insert into smoothies.public.orders(ingredients,name_on_order) values
     ('"""+ingredients_string+"""','"""+name_on_order+"""')"""
     insert_time=st.button('Submit Order')
